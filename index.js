aec74fe8867cb3140fb05b0f79adc1e7dfb3ab0b
Gabriel Rodriguez <gabriel.torres.rodriguez@gmail.com>
2017-09-11 18:37:13 -0500

    Deploy fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/example.html'));
 });
 
-var server = app.listen(3000);
+var server = app.listen(443);
 var io = socket(server);
 
 io.on('connection', function(socket){
